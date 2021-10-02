657a003cab3295122f9899e403ecaf5c5417e3e8
norivera <89235780+norivera@users.noreply.github.com>
2021-10-01 18:06:01 -0700

    try to impliment no repition of random numbers

--- magicsquare.c
+++ magicsquare.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>
 
 int sumRow(int array[][3]) {
     int value = 0;
@@ -49,26 +50,60 @@ int sumDiagonalTwo(int array[][3]) {
     return value;
 }
 
-void checkMagicSquare(int array[][3]) {
+bool checkMagicSquare(int array[][3]) {
     if (sumColumn(array) == 15 && sumRow(array) == 15 && sumDiagonalOne(array) == 15 && sumDiagonalTwo(array) == 15) {
-        printf("\nThe given array is Lo Shu Magic Square");
+        return true;
     } else {
-        printf("\nThe given array is not Lo Shu Magic Sqauare");
+        return false;
     }
 }
 
+int fisherYates() {
+    int staticArray[] = {1,2,3,4,5,6,7,8,9};
+    int array[3][3];
+    int x, temp;
+    int attempt = 0;
+
+    do {
+        int index = 0;
+        for (int i = 0; i < 9; i++) {
+            x = (rand() % 9) + 1;
+
+            temp = staticArray[i];
+            staticArray[i] = staticArray[x];
+            staticArray[x] = temp;
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0;j < 3; j++) {
+                array[i][j] = staticArray[index];
+                index++;
+
+                printf("%d", array[i][j]);
+            }
+            printf("\n");
+        }
+        attempt++;
+        printf("\nAttempt %d:\n", attempt);
+    } while(!checkMagicSquare(array));
+
+
+
+
+}
+
+
+
 int main() {
+    time_t t;
+    srand((unsigned) time(&t));
+
     int array[3][3] = { {4, 9, 2} , {3, 5, 7} , {8, 1, 6} };
 
-    printf("%d", sumRow(array));
-    printf("\n%d", sumColumn(array));
-    printf("\n%d", sumDiagonalTwo(array));
-    checkMagicSquare(array);
-
-    int test[3][3] = {{2, 7, 6}, {9, 5, 1}, {4, 3, 8}};
-    printf("\n%d", sumRow(test));
-    printf("\n%d", sumColumn(test));
-    printf("\n%d", sumDiagonalTwo(test));
-    checkMagicSquare(test);
+    //printf("%d", sumRow(array));
+    //printf("\n%d", sumColumn(array));
+    //printf("\n%d", sumDiagonalTwo(array));
+    //checkMagicSquare(array);
+    fisherYates();
 
 }
